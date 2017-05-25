bea8338476b390b5e5d015a323e3926273a181a5
Daniel Norman <danielnorman@ucla.edu>
2017-05-25 10:52:54 -0700

    Code style: spaces after comma

--- preprocessing/wm_mask.py
+++ preprocessing/wm_mask.py
@@ -5,7 +5,7 @@ import numpy as np
 if len(sys.argv) != 5:
   print 'Expects 4 arguments: threshold registered_wm_atlas_file mri_file output_file'
   quit()
-  
+
 threshold = float(sys.argv[1])
 
 wm_atlas = nib.load(sys.argv[2])
@@ -13,7 +13,7 @@ wm_atlas_data = wm_atlas.get_data()
 mri = nib.load(sys.argv[3])
 mri_data = mri.get_data()
 
-print 'Using %s with threshold > %s to mask %s' % (sys.argv[2],sys.argv[1],sys.argv[3])
+print 'Using %s with threshold > %s to mask %s' % (sys.argv[2], sys.argv[1], sys.argv[3])
 for i in xrange(len(mri_data)):
   for j in xrange(len(mri_data[0])):
     for k in xrange(len(mri_data[0][0])):
@@ -22,6 +22,6 @@ for i in xrange(len(mri_data)):
 
 # Save the masked result, which for some reason needs to be flipped across y axis
 # to match with input MRI
-extracted_mri = nib.Nifti1Image(mri_data, [[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+extracted_mri = nib.Nifti1Image(mri_data, [[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 nib.save(extracted_mri, sys.argv[4])
 print 'Saved as %s' % sys.argv[4]
