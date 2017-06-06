71bcd11dadbe4f3249b4ac208c91e7123fb92347
Daniel Norman <danielnorman@ucla.edu>
2017-06-06 00:05:11 -0700

    Change to only perform LBP on lesion area, not whole image

--- feature_extraction/lbp.py
+++ feature_extraction/lbp.py
@@ -2,13 +2,12 @@ import csv
 import sys
 import nibabel as nib
 import numpy as np
-import matplotlib.pyplot as plt
-import copy
 from skimage import feature
 
 NUM_POINTS = 8
-RADIUS = 1
-EXPAND_PIXELS = 10
+RADIUS = 2
+# Expand the bounding box so we include a little bit of region around the lesion when performing LBP
+EXPAND_PIXELS = RADIUS*2
 
 if len(sys.argv) != 4:
     print 'Expects 3 arguments: mri_file lesion_csv output_csv'
@@ -19,48 +18,27 @@ lesion_csv_file = sys.argv[2]
 output_csv_file = sys.argv[3]
 
 mri = nib.load(mri_file)
-mri_data = mri.get_data()
-print mri_data.shape # x, y, number of slices
+mri_data = mri.get_data()  # mri_data.shape = x, y, number of slices
+
 
 def extract_csv(input_file):
-    regions_of_interest = []
+    regions = []
     with open(input_file, 'rb') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             row_list = []
             for number in row:
                 row_list.append(int(number))
-            regions_of_interest.append(row_list)
-    return regions_of_interest
+            regions.append(row_list)
+    return regions
 
-def isolate_regions(input_arr, regions_of_interest, EXPAND_PIXELS):
-    count = 0
-    isolated_slices_array = np.zeros(shape=(len(input_arr), len(input_arr[0]), len(input_arr[0][0])))
-    for row in regions_of_interest:
-        # print row
-        for i in xrange(len(input_arr)):
-            for j in xrange(len(input_arr[0])):
-                for k in xrange(len(input_arr[0][0])):
-                    # check if slice number matches
-                    # checks if in bounding box in the order left, bottom, right, top
-                    if i == row[0] and j + EXPAND_PIXELS >= row[1] and k + EXPAND_PIXELS >= row[2] and j - EXPAND_PIXELS <= row[3] and k  - EXPAND_PIXELS <= row[4]:
-                        count += 1
-                        isolated_slices_array[i][j][k] = input_arr[i][j][k]
-    print "Total area of bounding boxes =", count
-    return input_arr
 
-def extract_image(input_arr, regions_of_interest):
-    all_images = []
-    all_hist = []
+def perform_lbp_on_regions(input_arr, regions):
+    histograms = []
+    for roi in regions:
+        region = input_arr[roi[1]-EXPAND_PIXELS:roi[3]+EXPAND_PIXELS, roi[2]-EXPAND_PIXELS:roi[4]+EXPAND_PIXELS, roi[0]]
 
-    # make it so array is (# slices, 256, 256)
-    input_arr = np.swapaxes(input_arr, 0, 2)
-    input_arr = np.swapaxes(input_arr, 1, 2)
-
-    isolated_slices_array = isolate_regions(input_arr, regions_of_interest, EXPAND_PIXELS)
-
-    for i in xrange(len(isolated_slices_array)):
-        lbp = feature.local_binary_pattern(isolated_slices_array[i], NUM_POINTS, RADIUS, method="uniform")
+        lbp = feature.local_binary_pattern(region, NUM_POINTS, RADIUS, method="uniform")
         (hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, NUM_POINTS + 3), range=(0, NUM_POINTS + 2))
 
         # normalize the histogram
@@ -68,22 +46,19 @@ def extract_image(input_arr, regions_of_interest):
         hist /= hist.sum()
 
         # return the histogram of Local Binary Patterns
-        all_hist.append(hist)
-        print hist
+        histograms.append(hist)
+    return histograms
 
-    return all_images, all_hist
 
-def flatten_histogram(all_hist):
-    return [sum(i) / len(all_hist) for i in zip(*all_hist)]
+def flatten_histogram(histograms):
+    return [sum(i) / len(histograms) for i in zip(*histograms)]
+
 
 regions_of_interest = extract_csv(lesion_csv_file)
 
-all_images, all_hist = extract_image(mri_data, regions_of_interest)
+all_hist = perform_lbp_on_regions(mri_data, regions_of_interest)
 
 flattened_hist = flatten_histogram(all_hist)
-
-print flattened_hist
-print len(all_hist)
 
 with open(output_csv_file, 'wb') as f:
     w = csv.writer(f)
