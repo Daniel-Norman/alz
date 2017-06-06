61cc8469f425f7e71ff77b53763307d0314c5eaa
Daniel Norman <danielnorman@ucla.edu>
2017-06-05 23:43:42 -0700

    Fix typo (cvs -> csv), and change input csv names

--- feature_extraction/lbp_batch.sh
+++ feature_extraction/lbp_batch.sh
@@ -2,14 +2,14 @@
 
 parallel=$1
 mri_folder=$2
-lesion_cvs_folder=$3
+lesion_csv_folder=$3
 output_folder=$4
 index=0
 for i in $(ls $mri_folder);
 do
     echo "Applying LBP to $i..."
     number=$(echo "$i" | tr -dc '0-9')
-    (python lbp.py "$mri_folder/$i" "$lesion_cvs_folder/lesion_$number.csv" "$output_folder/lbp_histogram_$number.csv" && echo "Done. Saved as $output_folder/lbp_histogram_$number.csv") &
+    (python lbp.py "$mri_folder/$i" "$lesion_csv_folder/lesions_masked_$number.nii.csv" "$output_folder/lbp_histogram_$number.csv" && echo "Done. Saved as $output_folder/lbp_histogram_$number.csv") &
     ((index++))
     if [ $index = $parallel ]; then
         index=0
